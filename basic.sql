386af59c5cd5ea4b816b83d84c8e6d1c37718d62
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2022-12-03 10:20:31 +0530

    mySql basic

---
+++ basic.sql
@@ -0,0 +1,64 @@
+show databases; 
+
+use classicmodels;
+
+show tables;
+
+use employees;
+
+select * from employees;
+
+select firstName,lastName from employees;
+
+
+select firstName from emplyees order by firstName ASC;
+
+select firstNAme from employees;
+
+select firstName from emplyees order by firstName DESC;
+
+select firstNAme from employees;
+
+select firstNAme from employees;
+
+show tables;
+
+use customers;
+
+select * from customers;
+
+/* multiple column we can access here *\
+
+SELECT
+	contactLastname,
+	contactFirstname,
+    phone ,
+    city
+    
+FROM
+	customers
+    
+ORDER BY
+    contactFirstname
+	contactLastname DESC
+    city ASC;
+    phone
+
+select contactFirstname , contactFirstName ,city from customers;
+SELECT city,
+contactLastName , contactFirstName
+FROM 
+customers 
+ORDER BY city ,contactLastName DESC , contactFirstname ASC;
+
+select *  from dual ;
+  
+use orderdetails;
+
+
+
+
+
+
+
+  
